a08c200e50e9fc3319a8f04acced61b2644cf1d2
Bo Tran <Botran612001@gmail.com>
2022-09-21 20:21:45 +0700

    Update smoke test

---
+++ test/smoke_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:my_app/main.dart';
+void main() {
+  testWidgets('smoke test ', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+  });
+}
